9bd55539e55dc254c33c54b87a59b1516f927f4b
David <desmaisons_david@hotmail.com>
2019-12-04 22:14:40 -0300

    Add count

--- example/index.js
+++ example/index.js
@@ -1,8 +1,18 @@
 import { App } from "../src/index";
 
-const render = state => `<h1>${state.name}</h1>`;
+const render = state => `<h1>${state.name}</h1>
+<p>${state.count}</p>`;
 const element = document.getElementById("app");
 
 const app = new App(element, render);
-console.log(app.render)
-app.render({ name: "hello" });
+const state = {
+  name: "hello",
+  count: 0
+};
+app.render(state);
+
+
+setInterval(() => {
+  state.count++;
+  app.render(state);
+}, 100);
