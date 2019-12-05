e874db031747bba399f26aaafede713623f662c6
David <desmaisons_david@hotmail.com>
2019-12-04 22:53:50 -0300

    Adding list

--- example/index.js
+++ example/index.js
@@ -1,18 +1,24 @@
 import { App } from "../src/index";
 
 const render = state => `<h1>${state.name}</h1>
-<p>${state.count}</p>`;
+<p>${state.count}</p>
+<ul>${state.array.map(value => `<li>${value}</li>`).join("")}</ul>`;
 const element = document.getElementById("app");
 
 const app = new App(element, render);
 const state = {
   name: "hello",
-  count: 0
+  count: 0,
+  array: []
 };
 app.render(state);
 
 
 setInterval(() => {
-  state.count++;
+  state.count++;  
+  if (state.count % 100 === 0) {
+    state.array = [];
+  }
+  state.array.push(state.count);
   app.render(state);
 }, 100);
