0564994054e56ad20583e14e92ddb51f317f1abb
David <desmaisons_david@hotmail.com>
2019-12-04 21:46:59 -0300

    First implementation

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 class App {
   constructor(element, render) {
+    this._render = render;
+    this._element = element;
   }
 
   render(state) {
+    const html = this._render(state);
+    this._element.innerHTML = html;
   }
 }
 
