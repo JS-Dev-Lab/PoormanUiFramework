64b70649189972f1bf76fd2d955feba7cd9e99f2
David Desmaisons <desmaisons_david@hotmail.com>
2019-12-05 12:58:04 -0300

    Implementation

--- src/index.js
+++ src/index.js
@@ -11,17 +11,18 @@ class UiEngine {
 
 class View {
   constructor(state, element, render) {
-    this._state = state;
+    this._state = { ...state};
     this._element = element;
     this._render = render;
+    window.state = state;
     const html = render(state);
     element.innerHTML = html;
   }
 
   update(updater) {
-    const state = { ...this._state };
-    updater(state);
-    return new View(state, this._element, this._render);
+    const newState = { ...this._state };
+    updater(newState);
+    return new View(newState, this._element, this._render);
   }
 }
 
