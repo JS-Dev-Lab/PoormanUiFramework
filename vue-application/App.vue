8a3ab8d8dbe10664b5f9cd169e31cb88cc7c370d
David Desmaisons <desmaisons_david@hotmail.com>
2019-12-06 14:17:06 -0300

    Adding vue project

---
+++ vue-application/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    Hello {{name}}
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "app",
+  data(){
+    return {
+      name: "vue"
+    }
+  }
+};
+</script>
+
+<style lang="less">
+html {
+  height: 100%;
+}
+
+body {
+  height: 100%;
+  overflow: hidden;
+}
+
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+}
+</style>
