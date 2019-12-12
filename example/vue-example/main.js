e4aca38dd2f1d494b4a6cab747e23fd421934bff
David Desmaisons <desmaisons_david@hotmail.com>
2019-12-12 14:45:53 -0300

    Clean-up

--- example/vue-example/main.js
+++
@@ -1,8 +0,0 @@
-import { UiEngine } from "./VueUiEngine";
-import App from "./App.vue";
-import { MyCountApplication } from "../application/app";
-
-const engine = new UiEngine("#app", App);
-const application = new MyCountApplication(engine);
-
-application.run();
