5d6d076fb6bf3767f17ed0abb48e24c3e9340bc9
David <desmaisons_david@hotmail.com>
2020-01-08 23:46:54 -0300

    Add basic render to vanilla package

--- packages/mvi.vanilla/src/index.js
+++ packages/mvi.vanilla/src/index.js
@@ -1,33 +1,4 @@
-function viewCreatorFactory(element, render) {
-  return ({ state, commands }) => {
-    return new View({ state: { ...state }, commands }, element, render);
-  };
-}
+import { viewCreatorFactory } from "./viewCreatorFactory"
+import {basicRender} from "./basicRender"
 
-class View {
-  constructor({ state, commands }, element, render) {
-    this._element = element;
-    this._commands = commands;
-    window.commands = commands;
-    this._render = render;
-    this.fullUpdate(state);
-  }
-
-  update(updater) {
-    const newState = { ...this._state };
-    updater(newState);
-    this.fullUpdate(newState);
-  }
-
-  get state() {
-    return this._state;
-  }
-
-  fullUpdate(state) {
-    this._state = Object.freeze(state);
-    const html = this._render({ state: this._state, commands: this._commands });
-    this._element.innerHTML = html;
-  }
-}
-
-export { viewCreatorFactory };
+export { viewCreatorFactory, basicRender };
